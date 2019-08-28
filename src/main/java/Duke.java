54040ba3ef545a18be504d07847bb989805f48a9
lattemadness <mapler098@gmail.com>
2019-08-28 22:26:33 +0800

    Level-3
    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,41 +1,80 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.PrintStream;
 
 public class Duke {
+
     public static void main(String[] args) {
+
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        ArrayList<String> info = new ArrayList<String>();
+
+        ArrayList<Task> info = new ArrayList<Task>();
         boolean end = false;
 
         while(!end)
         {
             Scanner in = new Scanner(System.in);
-            String s = in.nextLine();
+            String ss = in.nextLine();
 
 
-            if(s.equals("list"))
+            String[] s = ss.split(" ");
+
+
+            if(s[0].equals("list"))
             {
+                System.out.println("Here are the tasks in your list:");
+
                 int x = 1;
 
-                for(String i: info)
+                for(Task i: info)
                 {
-                    System.out.println(x +". "+ i);
+                   // PrintWriter consoleOut = new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"));
+                  //  consoleOut.println(filename);
+
+
+                    //PrintStream out = new PrintStream(System.out, true, "UTF-8");
+                    System.out.println(x + "." + i.printer());
+                   // consoleOut.println(x + "." + i.printer());
                     x++;
                 }
             }
 
-            else if(s.equals("bye"))
+            else if(s[0].equals("done"))
+            {
+             //   Scanner in = new Scanner(System.in);
+                //int b = s[2] - '0' ;
+                int b = Integer.valueOf(s[1]);
+                //int b = in.nextInt();
+
+                System.out.println("Nice! I've marked this task as done:");
+
+               // Task c = info.get(b-1);
+                if(b < 1 || b > info.size())
+                    System.out.println("Invalid. Please try again");
+                else {
+                    if(info.get(b-1).isDone)
+                        System.out.println("This task is already completed");
+                    else
+                    {
+                      info.get(b - 1).markAsDone();
+                      System.out.println(info.get(b - 1).printer());
+                    }
+                }
+            }
+
+            else if(s[0].equals("bye"))
             {
                 System.out.println("Bye. Hope to see you again soon!");
                 end = true;
             }
             else {
-                info.add(s);
+                Task obj = new Task(ss);
+                info.add(obj);
                 System.out.print("added: ");
-                System.out.println(s);
+                System.out.println(ss);
             }
         }
     }
