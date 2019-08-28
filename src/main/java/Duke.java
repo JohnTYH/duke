8f7d1e9f027b36fbd764ce53fdb77fcd499125fd
lattemadness <mapler098@gmail.com>
2019-08-28 16:21:57 +0800

    Level 1-1
    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+//import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,8 +8,28 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        boolean end = false;
+
+        while(!end)
+        {
+            Scanner in = new Scanner(System.in);
+            String s = in.nextLine();
+
+            if(s.equals("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+                end = true;
+            }
+            else
+                System.out.println(s);
+
+        }
     }
 
-    
+
 }
