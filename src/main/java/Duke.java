79c1d4d2c058b210de778c43c5ee659b01de1fb9
lattemadness <mapler098@gmail.com>
2019-08-29 14:31:29 +0800

    Level-5
    Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,6 +15,7 @@ public class Duke {
 
         boolean end = false;
 
+
         while(!end)
         {
             Scanner in = new Scanner(System.in);
@@ -22,45 +23,66 @@ public class Duke {
 
 
             String[] s = ss.split(" ",2);
+            boolean donewrong = false;
+            if(s.length == 1)
+                donewrong = true;
           // System.out.println(s[0]);
           // System.out.println(s[1]);
 
-            if(s[0].equals("todo"))
-            {
-                String[] todoArray = ss.split(" ",2);
-                Todo obj = new Todo(todoArray[1]);
-                info.add(obj);
+            if(s[0].equals("todo")) {
 
-                System.out.println("Got it. I've added this task:");
-                System.out.println(" " + obj.toString());
-                System.out.println("Now you have " + Integer.toString(Task.getTask()) + " task(s) in the list.");
+                try {
+               // if (donewrong) {
+                 //   System.out.println(" ☹ OOPS!!! The description of a todo cannot be empty.");
+              //  } else {
+                    String[] todoArray = ss.split(" ", 2);
+                    Todo obj = new Todo(todoArray[1]);
+                    info.add(obj);
 
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(" " + obj.toString());
+                    System.out.println("Now you have " + Integer.toString(Task.getTask()) + " task(s) in the list.");
+
+                //}
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(" ☹ OOPS!!! The description of a todo cannot be empty.");
+            }
             }
 
             else if(s[0].equals("deadline"))
             {
-                String[] DLarray = s[1].split("/by ");
-                Deadline obj = new Deadline(DLarray[0],DLarray[1]);
-                info.add(obj);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(" " + obj.toString());
-                System.out.println("Now you have " + Integer.toString(Task.getTask()) + " task(s) in the list.");
+                try{
+                    String[] DLarray = s[1].split("/by ");
+                    Deadline obj = new Deadline(DLarray[0], DLarray[1]);
+                    info.add(obj);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(" " + obj.toString());
+                    System.out.println("Now you have " + Integer.toString(Task.getTask()) + " task(s) in the list.");
+                    }
+                catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid. Please use '/by' keyword eg. (return book /by 1pm)");
+                }
 
             }
 
             else if(s[0].equals("event"))
             {
-                String[] Earray = s[1].split("/at ");
-                Event obj = new Event(Earray[0],Earray[1]);
-                info.add(obj);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(" " + obj.toString());
-                System.out.println("Now you have " + Integer.toString(Task.getTask()) + " task(s) in the list.");
-
+                try {
+                    String[] Earray = s[1].split("/at ");
+                    Event obj = new Event(Earray[0], Earray[1]);
+                    info.add(obj);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(" " + obj.toString());
+                    System.out.println("Now you have " + Integer.toString(Task.getTask()) + " task(s) in the list.");
+                    }
+                catch(ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid. Please use '/at' keyword eg. (event /at 1pm)");
+                }
             }
 
 
-            if(s[0].equals("list"))
+            else if(s[0].equals("list"))
             {
                 System.out.println("Here are the tasks in your list:");
 
@@ -72,51 +94,43 @@ public class Duke {
                     System.out.println(" " + x + ". " + info.get(i).toString());
                     x++;
                 }
-
-             /*   for(Task i: info)
-                {
-                   System.out.println(x + "." + i.printer());
-                   x++;
-                }*/
             }
 
 
 
-            else if(s[0].equals("done"))
-            {
-             //   Scanner in = new Scanner(System.in);
-                //int b = s[2] - '0' ;
-                int b = Integer.valueOf(s[1]);
-                //int b = in.nextInt();
+            else if(s[0].equals("done")) {
+                try {
 
+                    if (donewrong)
+                        System.out.println("Invalid. Please add a number");
+                    else {
 
-
-               // Task c = info.get(b-1);
-                if(b < 1 || b > info.size())
-                    System.out.println("Invalid. Please try again");
-                else {
-                    if(info.get(b-1).isDone)
-                        System.out.println("This task is already completed");
-                    else
-                    {
-                      System.out.println("Nice! I've marked this task as done:");
-                      info.get(b - 1).markAsDone();
-                      System.out.println(info.get(b - 1).printer());
+                        int b = Integer.valueOf(s[1]);
+                        if (b < 1 || b > info.size())
+                            System.out.println("Invalid. Please try again.");
+                        else {
+                            if (info.get(b - 1).isDone)
+                                System.out.println("This task is already completed.");
+                            else {
+                                System.out.println("Nice! I've marked this task as done:");
+                                info.get(b - 1).markAsDone();
+                                System.out.println(info.get(b - 1).printer());
+                            }
+                        }
                     }
                 }
+                catch (NumberFormatException e) {
+                    System.out.println("Invalid. Please try again.");
+                }
             }
-
             else if(s[0].equals("bye"))
             {
                 System.out.println("Bye. Hope to see you again soon!");
                 end = true;
             }
-     /*       else {
-                Task obj = new Task(ss);
-                info.add(obj);
-                System.out.print("added: ");
-                System.out.println(ss);
-            }*/
+            else {
+                 System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
         }
     }
 
